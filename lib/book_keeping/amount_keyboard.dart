012fc2396f5ab430326d0cd2dafb551e19d89a7c
chendk <chendk9@hotmail.com>
2020-08-27 22:47:56 +0800

    金额输入控制

--- lib/book_keeping/amount_keyboard.dart
+++ lib/book_keeping/amount_keyboard.dart
@@ -1,46 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:obk/utills/global_value.dart';
 
-class AmountKeyboard extends StatelessWidget {
-  final List<Widget> _icons = [
-    _createTextAmountButton("7"),
-    _createTextAmountButton("8"),
-    _createTextAmountButton("9"),
-    FlatButton(
-        textColor: Colors.black,
-        child: Text(_getTodayDate(), style: TextStyle(fontSize: 28, fontWeight: FontWeight.w400)),
-        onPressed: () {}),
-    _createTextAmountButton("4"),
-    _createTextAmountButton("5"),
-    _createTextAmountButton("6"),
-    _createTextAmountButton("+"),
-    _createTextAmountButton("1"),
-    _createTextAmountButton("2"),
-    _createTextAmountButton("3"),
-    _createTextAmountButton("—"),
-    _createTextAmountButton("."),
-    _createTextAmountButton("0"),
-    FlatButton(
-        child: SizedBox(
-            width: Global.screenWidth * 0.12,
-            child: Image(
-              image: AssetImage("asserts/images/backspace.png"),
-              fit: BoxFit.fitWidth,
-            )),
-        onPressed: () {}),
-    FlatButton(
-        color: Colors.blue,
-        textColor: Colors.white,
-        child: Text("确定", style: TextStyle(fontSize: 32, fontWeight: FontWeight.w400)),
-        onPressed: () {})
-  ];
+class AmountKeyboard extends StatefulWidget {
+  AmountKeyboard({Key key, @required this.onInput}) : super(key: key);
 
-  static FlatButton _createTextAmountButton(String text) {
+  static const String BACKSPACE = "backspace";
+  static const String CONFIRM = "confirm";
+
+  final ValueChanged<String> onInput;
+
+  @override
+  State<StatefulWidget> createState() => _AmountKeyboardState(onInput);
+}
+
+class _AmountKeyboardState extends State<AmountKeyboard> {
+  _AmountKeyboardState(this.onInput) : super();
+
+  final ValueChanged<String> onInput;
+
+  FlatButton _createTextAmountButton(String text) {
     return FlatButton(
-        child: Text(text, style: TextStyle(fontSize: 50, color: Colors.black, fontWeight: FontWeight.w400)), onPressed: () {});
+        child: Text(text, style: TextStyle(fontSize: 50, color: Colors.black, fontWeight: FontWeight.w400)),
+        onPressed: () => onInput(text));
   }
 
-  static String _getTodayDate() {
+  String _getTodayDate() {
     var today = DateTime.now();
     return "${today.year.toString()}-${today.month.toString().padLeft(2, '0')}-${today.day.toString().padLeft(2, '0')}";
   }
@@ -48,18 +32,50 @@ class AmountKeyboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-//      context为父级上下文biggest
+      // context为父级上下文biggest
       // 获取组件在父组件所能设置的最大高度
       var availableHeight = constraints.maxHeight;
       // 获取父组件宽度
       var availableWidth = constraints.maxWidth;
 
       return GridView(
+        physics: NeverScrollableScrollPhysics(),
         padding: EdgeInsets.all(0),
         gridDelegate:
             SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, childAspectRatio: availableWidth / availableHeight //显示区域宽高相等
                 ),
-        children: _icons,
+        children: [
+          _createTextAmountButton("7"),
+          _createTextAmountButton("8"),
+          _createTextAmountButton("9"),
+          FlatButton(
+              textColor: Colors.black,
+              child: Text(_getTodayDate(), style: TextStyle(fontSize: 28, fontWeight: FontWeight.w400)),
+              onPressed: () {}),
+          _createTextAmountButton("4"),
+          _createTextAmountButton("5"),
+          _createTextAmountButton("6"),
+          _createTextAmountButton("+"),
+          _createTextAmountButton("1"),
+          _createTextAmountButton("2"),
+          _createTextAmountButton("3"),
+          _createTextAmountButton("—"),
+          _createTextAmountButton("."),
+          _createTextAmountButton("0"),
+          FlatButton(
+              child: SizedBox(
+                  width: Global.screenWidth * 0.12,
+                  child: Image(
+                    image: AssetImage("asserts/images/backspace.png"),
+                    fit: BoxFit.fitWidth,
+                  )),
+              onPressed: () => onInput(AmountKeyboard.BACKSPACE)),
+          FlatButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              child: Text("确定", style: TextStyle(fontSize: 32, fontWeight: FontWeight.w400)),
+              onPressed: () => onInput(AmountKeyboard.CONFIRM))
+        ],
       );
     });
   }
--- lib/page/book_keeping.dart
+++ lib/page/book_keeping.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:obk/book_keeping/amount_keyboard.dart';
 import 'package:obk/book_keeping/category_slide_picker.dart';
+import 'package:obk/utills/global_value.dart';
 
-class BookKeepingPage extends StatelessWidget {
+class BookKeepingPage extends StatefulWidget {
+  BookKeepingPage({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BookKeepingPageState();
+}
+
+// 记账输入页
+class _BookKeepingPageState extends State<BookKeepingPage> {
+  final TextEditingController _amountController = TextEditingController();
+
+  void onAmountInput(String input) {
+    if (input.isEmpty) {
+      return;
+    }
+
+    var oldLength = _amountController.text.length;
+    if (AmountKeyboard.CONFIRM == input) {
+      // 计算金额
+    } else if (AmountKeyboard.BACKSPACE == input) {
+      // 退格
+      if (oldLength > 0) {
+        _amountController.text = _amountController.text.substring(0, oldLength - 1);
+      }
+    } else {
+      // 追加显示
+      _amountController.text += input;
+    }
+    var newLength = _amountController.text.length;
+    if (newLength != oldLength) {
+      _amountController.selection = new TextSelection(baseOffset: newLength, extentOffset: newLength);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var screenHeight = MediaQuery.of(context).size.height;
-    var screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       body: Column(
         children: [
@@ -18,12 +49,14 @@ class BookKeepingPage extends StatelessWidget {
             height: 70,
             color: Colors.red,
           ),
-          // 输入区
+          // 金额输入区
           Container(
             margin: EdgeInsets.only(top: 180),
             width: 400,
             height: 130,
-            child: _amountTextField(),
+            child: DecoratedBox(
+                decoration: BoxDecoration(border: Border(bottom: BorderSide(width: 4, color: Colors.grey[200]))),
+                child: AmountTextField(_amountController)),
           ),
           // 分类选择器
           Container(
@@ -33,15 +66,15 @@ class BookKeepingPage extends StatelessWidget {
           ),
           // 备注输入区
           Container(
-            margin: EdgeInsets.only(top: screenHeight * 0.02),
-            padding: EdgeInsets.only(left: screenWidth * 0.05),
-            height: screenHeight * 0.06,
+            margin: EdgeInsets.only(top: Global.screenHeight * 0.02),
+            padding: EdgeInsets.only(left: Global.screenWidth * 0.05),
+            height: Global.screenHeight * 0.06,
             color: const Color.fromRGBO(39, 144, 255, 0.08),
             child: Container(alignment: Alignment.centerLeft, child: _remarkTextField()),
           ),
           // 金额键盘
           Expanded(
-            child: AmountKeyboard(),
+            child: AmountKeyboard(onInput: onAmountInput),
           )
         ],
       ),
@@ -52,39 +85,50 @@ class BookKeepingPage extends StatelessWidget {
    * 备注输入区
    */
   Widget _remarkTextField() {
-    return TextField(
-      style: TextStyle(fontSize: 28),
-      cursorWidth: 3,
-      maxLines: 1,
-      decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: "点击此处添加备注",
-          hintStyle: TextStyle(fontSize: 28, color: Colors.grey),
-          prefixText: "备注：",
-          prefixStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.w400)),
-    );
+    return Row(children: [
+      Text("备注：", style: TextStyle(fontSize: 28, fontWeight: FontWeight.w100)),
+      Expanded(
+          child: TextField(
+              style: TextStyle(fontSize: 28),
+              cursorWidth: 3,
+              maxLines: 1,
+              decoration:
+                  InputDecoration(border: InputBorder.none, hintText: "点击此处添加备注", hintStyle: TextStyle(fontSize: 28, color: Colors.grey))))
+    ]);
   }
+}
 
-  /**
-   * 金额输入区
-   */
-  Widget _amountTextField() {
-    return TextField(
-      style: TextStyle(fontSize: 60),
-      cursorWidth: 6,
-      cursorColor: Colors.black,
-      readOnly: true,
-      showCursor: true,
-      autofocus: true,
-      maxLines: 1,
-      decoration: InputDecoration(
-          hintText: " 请输入金额",
-          hintStyle: TextStyle(fontSize: 60, color: Colors.grey),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-          prefixText: "￥",
-          prefixStyle: TextStyle(fontSize: 60, fontWeight: FontWeight.w600)),
-    );
+// 金额输入控件
+class AmountTextField extends StatefulWidget {
+  AmountTextField(this.textController, {Key key}) : super(key: key);
+
+  final textController;
+
+  @override
+  State<StatefulWidget> createState() => _AmountTextFieldState(textController);
+}
+
+class _AmountTextFieldState extends State<AmountTextField> {
+  _AmountTextFieldState(this.textController) : super();
+
+  final textController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text("￥", style: TextStyle(fontSize: 60, fontWeight: FontWeight.w600)),
+      Expanded(
+          child: TextField(
+              controller: textController,
+              style: TextStyle(fontSize: 60),
+              cursorWidth: 6,
+              cursorColor: Colors.black,
+              readOnly: true,
+              showCursor: true,
+              autofocus: true,
+              maxLines: 1,
+              decoration:
+                  InputDecoration(hintText: "请输入金额", hintStyle: TextStyle(fontSize: 60, color: Colors.grey), border: InputBorder.none)))
+    ]);
   }
 }
